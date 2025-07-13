dfaa81b4abe8390376dd021ed8731be4d9f4396d
LaboneeAktar <labonipub@gmail.com>
2025-07-13 09:36:56 +0600

    Create video card

--- scripts/video.js
+++ scripts/video.js
@@ -31,6 +31,23 @@ const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
   for (const video of videos) {
     console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
+      src=${video.thumbnail}
+      alt="Videos" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p></p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+   `;
+    videoContainer.appendChild(card);
   }
 };
 
