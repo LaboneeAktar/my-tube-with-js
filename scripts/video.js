6cd455ce88f9a69b71786de0ed7f34859eadb9f0
LaboneeAktar <labonipub@gmail.com>
2025-07-13 10:07:00 +0600

    Designed Video Card

--- scripts/video.js
+++ scripts/video.js
@@ -35,17 +35,29 @@ const displayVideos = (videos) => {
     card.classList = "card card-compact";
     card.innerHTML = `
     <figure class="h-[200px]">
-    <img class="h-full w-full object-cover"
-      src=${video.thumbnail}
-      alt="Videos" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p></p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+      <img class="h-full w-full object-cover"
+        src=${video.thumbnail}
+        alt="Videos" />
+    </figure>
+    <div class="px-0 py-2 flex gap-2">
+      <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${
+          video.authors[0].profile_picture
+        } />
+      </div>
+      <div>
+        <h2 class="font-bold text-lg">${video.title}</h2>
+        <div class="flex items-center">
+          <p class="text-gray-400 text-sm">${video.authors[0].profile_name}</p>
+            ${
+              video.authors[0].verified == true
+                ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""/>`
+                : ``
+            }
+
+        </div>
+      </div>
     </div>
-  </div>
    `;
     videoContainer.appendChild(card);
   }
