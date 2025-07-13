86ec3283d656e937fd1f61e98fb10ba2e1c0cf06
LaboneeAktar <labonipub@gmail.com>
2025-07-13 11:22:29 +0600

    Add time in video thumbnail

--- scripts/video.js
+++ scripts/video.js
@@ -1,13 +1,12 @@
 // Fetch Data and show categories
-
 const loadVideoCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayVideoCategories(data.categories))
     .catch((error) => console.error("Something Error", error));
 };
-// Fetch Data and show videos
 
+// Fetch Data and show videos
 const loadVideos = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
@@ -34,10 +33,18 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure class="h-[200px]">
+    <figure class="h-[200px] relative">
       <img class="h-full w-full object-cover"
         src=${video.thumbnail}
-        alt="Videos" />
+        alt="video_thumbnail" />
+        ${
+          video.others.posted_date?.length == 0
+            ? ""
+            : `<span
+            class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
+            >${getTimeString(video.others.posted_date)}</span>`
+        }
+        
     </figure>
     <div class="px-0 py-2 flex gap-2">
       <div>
@@ -60,6 +67,28 @@ const displayVideos = (videos) => {
     </div>
    `;
     videoContainer.appendChild(card);
+  }
+};
+
+// Get time - hour, minute, second
+const getTimeString = (time) => {
+  const day = parseInt(time / 86400);
+  let remainingMinute = time % 86400;
+  const hour = parseInt(remainingMinute / 3600);
+  let remainingSecond = remainingMinute % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = minute % 60;
+
+  if (day != 0 && hour != 0 && minute != 0 && remainingSecond != 0) {
+    return `${day} days ${hour} hrs ${minute} mins ${remainingSecond} s ago`;
+  } else if (day == 0 && hour != 0 && minute != 0 && remainingSecond != 0) {
+    return `${hour} hrs ${minute} mins ${remainingSecond} s ago`;
+  } else if (day == 0 && hour == 0 && minute != 0 && remainingSecond != 0) {
+    return `${minute} mins ${remainingSecond} s ago`;
+  } else if (day == 0 && hour == 0 && minute == 0 && remainingSecond != 0) {
+    return `${remainingSecond} s ago`;
+  } else {
+    return `${remainingMinute}`;
   }
 };
 
